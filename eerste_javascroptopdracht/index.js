9a8400e053bef6bfd3ccc471e6ed0e54ab16b734
Booren, Mark van -Marky9000 <71060851+Marky9000@users.noreply.github.com>
2020-10-30 09:35:34 +0100

    Add files via upload

---
+++ eerste_javascroptopdracht/index.js
@@ -0,0 +1,12 @@
+console.log("Hello Winc Academy")
+let Mark = 'Wincstudent';
+let Markleeftijd = 46;
+console.log('Mark');
+console.log(4 + 4);
+console.log("4" + "4");
+console.log(433344 + 37837474);
+console.log(14 / 3);
+console.log(1.00229839839393993939 % 2);
+let Markleeftijd2 = ('46');
+typeof '46';
+typeof 46;
