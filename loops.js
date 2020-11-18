979b378edd1fb3326f075200299ca6fb9c6ff95f
Booren, Mark van -Marky9000 <71060851+Marky9000@users.noreply.github.com>
2020-11-18 07:42:05 +0100

    Add files via upload
    opdracht loops oude en nieuwe stijl

---
+++ loops.js
@@ -0,0 +1,13 @@
+
+const colours = ["Yello","Blue", "Red", "Orange"];
+
+i=0;
+while (colours[i]){
+console.log(colours[i]);
+i++; }
+
+
+for (let i=0; i<colours.length; i++){
+console.log(colours[i]);}
+
+colours.forEach((L)=>console.log(L));
