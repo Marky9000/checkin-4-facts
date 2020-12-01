b59442e41882f87a9977f85e9f6c987e2f29f262
Booren, Mark van -Marky9000 <71060851+Marky9000@users.noreply.github.com>
2020-12-01 21:26:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
+// let fruits = ['appel','banana', 'grape']
+// console.log(fruits.length)
+
+// let first = fruits[0]
+
+// console.log(first);
+
+// let last = fruits[fruits.length -1]
+
+// console.log(last); 
+// // fruits.foreach(function(item, index, array)
+
+// fruits.forEach(function(item, index, array){
+//   console.log(item, index)
+// })
+
+// let newlength = fruits.push('orange')
+
+// // console.log(fruits);
+
+// let array= ['vet','gaaf', 'crazy']
+
+// const coolerbij = array.push('cool');
+// // console.log(array.length);
+
+// let Benelux=["Belgie", "Nederland", "Luxemburg"]
+
+// console.log(Benelux[0]);
+// // resultaat: "Belgie" 
+// let dieren =["Haas", "Cavia", "Kip", "Schildpad"]
+
+// console.log(dieren[dieren.length -1]);
+
+// const presidents = ["Trump", "Obama", "Bush", "Clinton"];
 
 
+// // console.log(presidents.slice(1));
 
-// Deze functie 'performs a task' 
-function greet1() {
-  console.log("Hey hoi")
-}
-// Deze functie 'returns a value'
-function greet2() {
-  return "Hallo!"
-}
+// console.log(presidents.splice(1,3));
+
+// combineArrays([1,2,3], [4,5,6]) 
+// // resultaat: [1,2,3,4,5,6]
+
+// const cars = [1,2,3];
+// const trucks = [4,5,6];
+// const combined = cars.concat(trucks);
+// console.log(combined);
+
+let arrayWinc = ['Winc', 'Academy', 'is', 'leuk' ]
+
+  console.log(arrayWinc.join(' '))
 
